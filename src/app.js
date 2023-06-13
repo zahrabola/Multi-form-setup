2c1e2c59dca7152a9091b2807acd1ac263903650
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-06-13 21:48:55 +0100

    update

---
+++ src/app.js
@@ -0,0 +1,20 @@
+
+const form1 = document.querySelector(".1-form");
+const form2 = document.querySelector(".2-form");
+const form3 = document.querySelector(".3-form");
+
+const btn1_form = document.querySelector(".btn1-form");
+const btn2_form = document.querySelector(".btn2-form");
+const btn3_form = document.querySelector(".btn3-form");
+
+const btn1_form_next = document.querySelector(".btn1-form .btn-next");
+const btn2_form_back = document.querySelector(".btn2-form .btn-back");
+const btn2_form_next = document.querySelector(".btn2-form .btn-next");
+const btn3_form_next = document.querySelector(".btn3-form .btn-back");
+
+const f2progressbar = document.querySelector(".f2progressbar");
+const f3progressbar = document.querySelector(".f3progressbar");
+
+const btn_done = document.querySelector(".btn-done");
+const modalwrapper = document.querySelector(".modal-wrapper");
+const shadow = document.querySelector(".shadow");
